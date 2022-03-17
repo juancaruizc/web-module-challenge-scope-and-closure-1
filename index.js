dada350cf4a006cc07e9cf094b364af7419f39d9
Juan Ruiz <juanruiz7201@gmail.com>
2022-03-17 13:39:25 -0700

    completed all tasks

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
- * 
+ *
  * Use the higher order function processFirstItem below to do the following:
  *  1. Receive an array of strings in a parameter
  *  2. Receive a callback function that takes a string as its argument in a parameter
- *  3. Return the result of invoking the callback function and passing in the FIRST 
+ *  3. Return the result of invoking the callback function and passing in the FIRST
  *     element in the array as the argument
- * 
+ *
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
-*/
+ */
 
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
-console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(
+  processFirstItem(["foo", "bar"], function (str) {
+    return str + str;
+  })
+);
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
@@ -28,19 +31,22 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  // - counter 1 has the variable in its local scope which means it will reset the counter everytime the function is invoked
+
   2. Which of the two uses a closure? How can you tell?
-  
+  // - counter1 uses a closure because it has a nested function which reaches out to its parent for context
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better? 
+// - counter1 would be preferable if we wanted to keep the counter local to the function, counter 2 would be better if we wanted to keep the counter globally avaliable and wont reset every time the function counterMaker is ran
 */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -52,7 +58,6 @@ function counter2() {
   return count++;
 }
 
-
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
   1. Return a random whole number of points between 0 and 2 scored by one team in an inning
@@ -62,10 +67,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning() {
+  return Math.floor(Math.random() * Math.floor(3));
 }
-
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -79,21 +83,38 @@ Use the finalScore function below to do the following:
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, totalInnings) {
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for (let i = 0; i < totalInnings; i++) {
+    homeScore += inningCB();
+    awayScore += inningCB();
+  }
+
+  return {
+    Home: homeScore,
+    Away: awayScore,
+  };
 }
+
+// console.log(finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    Home: inningCB(),
+    Away: inningCB(),
+  };
 }
 
+// console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -136,17 +157,41 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScoreCB, inningCB, totalInnings) {
+  const finalScoreBoard = [];
+  let totalHomeScore = 0;
+  let totalAwayScore = 0;
+
+  for (let i = 0; i < totalInnings; i++) {
+    let currentHomeScore = getInningScoreCB(inningCB).Home;
+    let currentAwayScore = getInningScoreCB(inningCB).Away;
+
+    totalHomeScore += currentHomeScore;
+    totalAwayScore += currentAwayScore;
+
+    finalScoreBoard.push(
+      `Inning ${i + 1}: Away ${currentAwayScore} - Home ${currentHomeScore}`
+    );
+  }
+
+  if (totalHomeScore === totalAwayScore) {
+    finalScoreBoard.push(
+      `This game will require extra innings: Away ${totalAwayScore} - Home ${totalHomeScore}`
+    );
+  } else {
+    finalScoreBoard.push(
+      `Final Score: Away ${totalAwayScore} - Home ${totalHomeScore}`
+    );
+  }
+  return finalScoreBoard;
 }
 
-
-
+console.log(scoreboard(getInningScore, inning, 9));
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working');
-  return 'bar';
+function foo() {
+  console.log("its working");
+  return "bar";
 }
 foo();
 module.exports = {
@@ -158,4 +203,4 @@ module.exports = {
   finalScore,
   getInningScore,
   scoreboard,
-}
+};
